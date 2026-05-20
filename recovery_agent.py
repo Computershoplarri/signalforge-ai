56c31f0159f52078f36ff0f20125d1c1b62a4293
Computershoplarri <computershop768@gmail.com>
2026-05-20 15:46:04 +0500

    Delete recovery_agent.py

--- recovery_agent.py
+++
@@ -1,9 +0,0 @@
-def recover(results):
-
-    recovery = []
-
-    for r in results:
-        if r["status"] == "FAILED":
-            recovery.append("Fallback supplier activated")
-
-    return recovery
